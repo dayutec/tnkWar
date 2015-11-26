7b7ad97488f2a499ec76cfde1e74c0878551366f
dayutec <a2014521@aim.com>
2015-11-26 12:53:02 +0800

    0.4

--- tankWar/src/tankWar/tankClinet.java
+++ tankWar/src/tankWar/tankClinet.java
@@ -8,17 +8,20 @@ import java.awt.event.WindowEvent;
 
 public class tankClinet extends Frame {
 
+	int tank_x=50, tank_y=50;
 	
 	public void paint(Graphics g){
 		Color c = g.getColor();
 		g.setColor(Color.RED);
-		g.fillOval(50, 50, 30, 30);
+		g.fillOval(tank_x, tank_y, 30, 30);
 		g.setColor(c);
+		
+		tank_y += 5; //tanks_y
 	}
 	
 	
 	public void lauchFrame(){
-		this.setLocation(400,300);
+		this.setLocation(200,50);
 		this.setSize(800, 600);
 		this.setTitle("TankWar");
 		
@@ -34,6 +37,8 @@ public class tankClinet extends Frame {
 		this.setResizable(false);
 		this.setBackground(Color.GRAY);
 		setVisible(true);
+		
+		new Thread( new PaintThread()).start();
 	}
 	
 	
@@ -41,6 +46,29 @@ public class tankClinet extends Frame {
 		// TODO Auto-generated method stub
 		tankClinet tc = new tankClinet();
 		tc.lauchFrame();
+		
 	}
+	
+	
+	private class PaintThread implements Runnable{
+
+		@Override
+		public void run() {
+			// TODO Auto-generated method stub
+			while(true){
+				repaint();
+				try {
+					Thread.sleep(50);
+				} catch (InterruptedException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
+		}
+		
+		
+	}
+	
+	
 
 }
