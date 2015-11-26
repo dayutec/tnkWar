fa94b5fb3b3b541a1aa9052dddf9f415b6750ef4
dayutec <a2014521@aim.com>
2015-11-26 12:25:22 +0800

    0.3

--- tankWar/src/tankWar/tankClinet.java
+++ tankWar/src/tankWar/tankClinet.java
@@ -1,6 +1,8 @@
 package tankWar;
 
 import java.awt.Frame;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
 
 public class tankClinet extends Frame {
 
@@ -8,6 +10,18 @@ public class tankClinet extends Frame {
 	public void lauchFrame(){
 		this.setLocation(400,300);
 		this.setSize(800, 600);
+		this.setTitle("TankWar");
+		
+		this.addWindowListener(new WindowAdapter(){
+			@Override
+			public void windowClosing(WindowEvent e) {
+				// TODO Auto-generated method stub
+				super.windowClosing(e);
+				System.exit(0);
+			}
+		});
+		
+		this.setResizable(false);
 		setVisible(true);
 	}
 	
