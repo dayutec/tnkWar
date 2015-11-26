2d90107e6dfd466edbf80cfacb7ba4f1ef68a4e7
dayutec <a2014521@aim.com>
2015-11-26 12:38:13 +0800

    0.3

--- tankWar/src/tankWar/tankClinet.java
+++ tankWar/src/tankWar/tankClinet.java
@@ -1,11 +1,21 @@
 package tankWar;
 
+import java.awt.Color;
 import java.awt.Frame;
+import java.awt.Graphics;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 
 public class tankClinet extends Frame {
 
+	
+	public void paint(Graphics g){
+		Color c = g.getColor();
+		g.setColor(Color.RED);
+		g.fillOval(50, 50, 30, 30);
+		g.setColor(c);
+	}
+	
 	
 	public void lauchFrame(){
 		this.setLocation(400,300);
@@ -22,6 +32,7 @@ public class tankClinet extends Frame {
 		});
 		
 		this.setResizable(false);
+		this.setBackground(Color.GRAY);
 		setVisible(true);
 	}
 	
