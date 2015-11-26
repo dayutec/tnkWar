e122e172573999810a35f92123be297e4bbad439
dayutec <a2014521@aim.com>
2015-11-26 13:34:04 +0800

    0.4.1

--- tankWar/src/tankWar/tankClinet.java
+++ tankWar/src/tankWar/tankClinet.java
@@ -3,6 +3,7 @@ package tankWar;
 import java.awt.Color;
 import java.awt.Frame;
 import java.awt.Graphics;
+import java.awt.Image;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 
@@ -10,6 +11,9 @@ public class tankClinet extends Frame {
 
 	int tank_x=50, tank_y=50;
 	
+	Image offScreenImage = null;
+
+
 	public void paint(Graphics g){
 		Color c = g.getColor();
 		g.setColor(Color.RED);
@@ -17,6 +21,26 @@ public class tankClinet extends Frame {
 		g.setColor(c);
 		
 		tank_y += 5; //tanks_y
+	}
+	
+	
+	@Override
+	public void update(Graphics g) {
+		// TODO Auto-generated method stub
+		super.update(g);
+		if(offScreenImage == null){
+			offScreenImage = this.createImage(800,600);
+		}
+		Graphics gOffScreen = offScreenImage.getGraphics();
+		
+		Color c = gOffScreen.getColor();
+		gOffScreen.setColor(Color.GREEN);
+		gOffScreen.fillRect(0, 0, 800, 600);
+		gOffScreen.setColor(c);
+		
+		paint(gOffScreen);
+		g.drawImage(offScreenImage, 0, 0,null);
+		
 	}
 	
 	
