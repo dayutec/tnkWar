fe7ed02cb3b5a1b9a1e058bae25e30be83ee0365
dayutec <a2014521@aim.com>
2015-11-27 13:57:25 +0800

    0.8

--- tankWar/src/tankWar/Tank.java
+++ tankWar/src/tankWar/Tank.java
@@ -5,7 +5,15 @@ import java.awt.Graphics;
 import java.awt.event.KeyEvent;
 
 public class Tank {
-	int loc_x,loc_y;
+	
+	public static final int XSPEED=5;
+	public static final int YSPEED=5;
+	
+	private int loc_x,loc_y;
+	private boolean bL= false ,bU =false, bD= false, bR=false;
+	
+	private enum Direction{L,LU,U,RU,R,RD,D,LD,S};
+	private Direction dir = Direction.S;
 
 	public Tank(int loc_x, int loc_y) {
 		super();
@@ -18,18 +26,51 @@ public class Tank {
 		g.setColor(Color.RED);
 		g.fillOval(loc_x, loc_y, 30, 30);
 		g.setColor(c);
+		
+		move();
+	}
+	
+	
+	public void move(){
+		switch (dir){
+		case L: loc_x-=XSPEED; break;
+		case LU:loc_x-=XSPEED; loc_y-=YSPEED;break;
+		case U: loc_y-=YSPEED;break;
+		case RU:loc_x+=XSPEED; loc_y-=YSPEED;break;
+		case R: loc_x+=XSPEED; break;
+		case RD:loc_x+=XSPEED; loc_y+=YSPEED;break;	
+		case D: loc_y+=YSPEED;break;
+		case LD:loc_x-=XSPEED; loc_y+=YSPEED;break;
+		case S: break;
+			default : break;
+		}
 	}
 	
 	public void keypress(KeyEvent e){
 		int key = e.getKeyCode();
 		switch (key){
-			case KeyEvent.VK_RIGHT : loc_x+=50; break;
-			case KeyEvent.VK_LEFT  : loc_x-=50; break;
-			case KeyEvent.VK_UP    : loc_y-=50; break;
-			case KeyEvent.VK_DOWN  : loc_y+=50; break;
+			case KeyEvent.VK_RIGHT : bR = true; break;
+			case KeyEvent.VK_LEFT  : bL = true; break;
+			case KeyEvent.VK_UP    : bU = true; break;
+			case KeyEvent.VK_DOWN  : bD = true; break;
 			
 			default :break;
 		}
+		
+		locateDirection();
 	} 
+	
+	void locateDirection(){
+		if(bL && !bU && !bR && !bD) dir = Direction.L ;
+		else if( bL &&  bU && !bR && !bD) dir = Direction.LU ;
+		else if(!bL && bU && !bR && !bD)  dir = Direction.U ;
+		else if(!bL && bU && bR && !bD)   dir = Direction.RU ;
+		else if(!bL && !bU && bR && !bD)  dir = Direction.R ;
+		else if(!bL && !bU && bR && bD)   dir = Direction.RD ;
+		else if(!bL && !bU && !bR && bD)  dir = Direction.D ;
+		else if( bL && !bU && !bR && bD)  dir = Direction.LD ;
+		else if(!bL && !bU && !bR && !bD) dir = Direction.S ;
+		
+	}
 	
 }
